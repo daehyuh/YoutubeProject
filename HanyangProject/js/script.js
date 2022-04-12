0e3d75b0399081ae308eabc71997613b64df7154
대현 <rkdeown10@naver.com>
2022-04-12 15:07:56 +0900

    Add files via upload

---
+++ HanyangProject/js/script.js
@@ -0,0 +1,10 @@
+function test (){
+    document.body.classList.toggle('dark-theme');
+}
+
+
+
+$(document).ready(function(){
+    $("#user").removeClass("user-hidden");
+    $("#user").addClass("user");
+  });
