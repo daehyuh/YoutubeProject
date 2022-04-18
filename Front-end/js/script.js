4f020975609b6427bc1aa27604e9ef64ea501326
daehyuh0318 <rkdeown10@naver.com>
2022-04-18 17:05:32 +0900

    commit

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -1,5 +1,12 @@
 function test(){
-    document.body.classList.toggle('dark-theme');
+
+    if ( document.body.classList.contains('white-theme') ){
+        document.body.classList.replace('white-theme', 'dark-theme');
+        document.getElementById("darkmode").innerText = "화이트 테마";
+    } else{
+        document.body.classList.replace('dark-theme', 'white-theme');
+        document.getElementById("darkmode").innerText = "다크 테마";
+    }
 }
 
 function test2(){
