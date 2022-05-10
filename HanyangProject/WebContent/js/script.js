88327c947b85e23e4e69f1b479a2f13e4bc8b4ed
대현 <rkdeown10@naver.com>
2022-05-10 14:10:36 +0900

    Update script.js

--- HanyangProject/WebContent/js/script.js
+++ HanyangProject/WebContent/js/script.js
@@ -1,12 +1,11 @@
 function test(){
 
-    if ( document.body.classList.contains('white-theme') ){
-        document.body.classList.replace('white-theme', 'dark-theme');
-        document.getElementById("darkmode").innerText = "화이트 테마";
-    } else{
-        document.body.classList.replace('dark-theme', 'white-theme');
-        document.getElementById("darkmode").innerText = "다크 테마";
-    }
+	if ( document.body.classList.contains('white-theme') ){
+		 document.body.classList.replace('white-theme', 'dark-theme');
+	} else{
+		document.body.classList.replace('dark-theme', 'white-theme');
+	}
+
 }
 function test2(){
     if ( document.getElementById("user").classList.contains('userhidden') ){
