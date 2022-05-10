be616457885c72cbed3f015804fd974028adac2e
대현 <rkdeown10@naver.com>
2022-05-10 14:02:02 +0900

    Update script.js

--- HanyangProject/WebContent/js/script.js
+++ HanyangProject/WebContent/js/script.js
@@ -1,7 +1,13 @@
 function test(){
-    document.body.classList.toggle('dark-theme');
-}
 
+    if ( document.body.classList.contains('white-theme') ){
+        document.body.classList.replace('white-theme', 'dark-theme');
+        document.getElementById("darkmode").innerText = "화이트 테마";
+    } else{
+        document.body.classList.replace('dark-theme', 'white-theme');
+        document.getElementById("darkmode").innerText = "다크 테마";
+    }
+}
 function test2(){
     if ( document.getElementById("user").classList.contains('userhidden') ){
         document.getElementById("user").classList.replace('userhidden', 'user');
