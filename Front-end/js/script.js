1d1c78d158f146a601be54857918b4131616ae41
HG04 <74530048+hg04hg@users.noreply.github.com>
2022-05-12 10:35:50 +0900

    Update script.js

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -1,12 +1,11 @@
 function test(){
 
-    if ( document.body.classList.contains('white-theme') ){
-        document.body.classList.replace('white-theme', 'dark-theme');
-        document.getElementById("darkmode").innerText = "화이트 테마";
-    } else{
-        document.body.classList.replace('dark-theme', 'white-theme');
-        document.getElementById("darkmode").innerText = "다크 테마";
-    }
+	if ( document.body.classList.contains('white-theme') ){
+		 document.body.classList.replace('white-theme', 'dark-theme');
+	} else{
+		document.body.classList.replace('dark-theme', 'white-theme');
+	}
+
 }
 
 function test2(){
@@ -14,5 +13,53 @@ function test2(){
         document.getElementById("user").classList.replace('userhidden', 'user');
     } else{
         document.getElementById("user").classList.replace('user', 'userhidden');
+    }
+}
+
+function check(){
+	
+	var user_id = document.getElementById("user_id");
+	var user_pwd = document.getElementById("user_pwd");
+	var user_pwd_check = document.getElementById("user_pwd_check");
+	var user_name = document.getElementById("user_name");
+	
+	if(user_id.value == "" || user_id.value == null){s
+		alert("아이디를 입력하세요.");
+		user_id.focus();
+		return;
+	} else if (user_pwd.value == "" || user_pwd.value == null){
+		alert("비밀번호를 입력하세요.");
+		user_pwd.focus();
+		return;
+	} else if (user_pwd_check.value == user_pwd || user_pwd_check.value == "" || user_pwd_check.value == null){
+		alert("비밀번호가 일치하지 않습니다.");
+		user_pwd_check.focus();
+		return;
+	} else if(user_name.value == "" || user_name.value == null){ 
+		alert("이름을 입력하세요.");
+		user_name.focus();
+		return;
+	}
+	form.method="post";
+	form.action="/JOINUS";
+	form.submit();
+	
+}
+
+var comment_flag = true;
+
+function hidden_comment(){
+    var hiddenComment = document.getElementById("hidden_comment");
+    if (comment_flag == true){
+        if (hiddenComment.style.display == "none"){
+            hiddenComment.style.display = "block";
+            comment_flag = false;
+        }
+    }
+    else if (comment_flag == false){
+        if (hiddenComment.style.display == "block"){
+            hiddenComment.style.display = "none";
+            comment_flag = true;
+        } 
     }
 }
