ec46981e6b193d1ebf4f7725ee33c5254f0b8a5e
HG04 <74530048+hg04hg@users.noreply.github.com>
2022-05-13 17:48:25 +0900

    HanSeok commit 2022 / 05/ 13 pm5:48

---
+++ HanyangProject/src/servlet/Video2.java
@@ -0,0 +1,57 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.VideoDAO;
+import entity.VideoEntity;
+
+/**
+ * Servlet implementation class Video2
+ */
+@WebServlet("/Video2")
+public class Video2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Video2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uuid = request.getParameter("video_uuid");
+		System.out.println(uuid);
+		List<VideoEntity> Videolist = new ArrayList<VideoEntity>();
+		Videolist = VideoDAO.VideoSelectAllByuuId(uuid);
+		
+		request.setAttribute("Video", Videolist);
+		
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatcher = context.getRequestDispatcher("/jsp/video.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
